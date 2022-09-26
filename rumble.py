de0160d7b44dcf94d06c12c2d4fe1239300d291b
✠ Gregory L. Dietsche ✠ <gregory.dietsche@cuw.edu>
2022-09-26 10:39:45 -0500

    reconnect always

--- rumble.py
+++ rumble.py
@@ -14,7 +14,6 @@ parser.add_argument('--cert-key', nargs='?',  dest='certkey', default=None, help
 parser.add_argument('--channel', nargs='?',  dest='channel', default='', help='the channel to join')
 parser.add_argument('--password', nargs='?',  dest='password', default='' ,help='mumble server password')
 parser.add_argument('--port', nargs='?',  dest='port', default=64738, help='the server to connect to (default "64738")')
-parser.add_argument('--reconnect',  dest='reconnect', action='store_true', help='reconnect if the server connection is lost')
 parser.add_argument('--server', nargs='?',  dest='server', default='localhost', help='the server to connect to (default "localhost")')
 parser.add_argument('--username', dest='username', help='the username of the client')
 args = parser.parse_args()
@@ -28,22 +27,18 @@ def OnConnected():
 def OnDisconnected():
     IsConnected.clear()
     print("Server Disconnected.")
-
-    if args.reconnect:
-        print("Attempting to reconnect...")
+    print("Attempting to reconnect...")
 
 pyAudioBufferSize=1024
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=48000, input=True, output=False, frames_per_buffer=pyAudioBufferSize)
 
-mumble = pymumble_py3.Mumble(args.server, args.username, password=args.password, port=args.port, certfile=args.certfile, keyfile=args.certkey, reconnect=args.reconnect)
+mumble = pymumble_py3.Mumble(args.server, args.username, password=args.password, port=args.port, certfile=args.certfile, keyfile=args.certkey, reconnect=True)
 mumble.set_application_string("Rumble de K9CTS")
 mumble.start()
 mumble.callbacks.set_callback(PYMUMBLE_CLBK_CONNECTED, OnConnected)
 mumble.callbacks.set_callback(PYMUMBLE_CLBK_DISCONNECTED, OnDisconnected)
 mumble.is_ready()
-
-# mumble.channels.find_by_name(args.channel).move_in()
 
 while True:
     data = stream.read(pyAudioBufferSize, exception_on_overflow=False)
