20d639ea9c1e6ab61af175900df20621badf8a83
✠ Gregory L. Dietsche ✠ <gregory.dietsche@cuw.edu>
2022-09-26 10:53:08 -0500

    cleanup

--- rumble.py
+++ rumble.py
@@ -7,40 +7,58 @@ import pyaudio
 import pymumble_py3
 from pymumble_py3.constants import *
 from threading import Thread, Event
+import signal
 
-parser = argparse.ArgumentParser(prog='rumble', description='rumble streams audio from your microphone input')
-parser.add_argument('--cert-file', nargs='?',  dest='certfile', default=None, help='PEM encoded public key certificate')
-parser.add_argument('--cert-key', nargs='?',  dest='certkey', default=None, help='PEM encoded private key certificate')
-parser.add_argument('--channel', nargs='?',  dest='channel', default='', help='the channel to join')
-parser.add_argument('--password', nargs='?',  dest='password', default='' ,help='mumble server password')
-parser.add_argument('--port', nargs='?',  dest='port', default=64738, help='the server to connect to (default "64738")')
-parser.add_argument('--server', nargs='?',  dest='server', default='localhost', help='the server to connect to (default "localhost")')
-parser.add_argument('--username', dest='username', help='the username of the client')
-args = parser.parse_args()
+argParser = argparse.ArgumentParser(prog='rumble', description='rumble streams audio from your microphone input')
+argParser.add_argument('--cert-file', nargs='?',  dest='certfile', default=None, help='PEM encoded public key certificate')
+argParser.add_argument('--cert-key', nargs='?',  dest='certkey', default=None, help='PEM encoded private key certificate')
+argParser.add_argument('--channel', nargs='?',  dest='channel', default='', help='the channel to join')
+argParser.add_argument('--password', nargs='?',  dest='password', default='' ,help='mumble server password')
+argParser.add_argument('--port', nargs='?',  dest='port', default=64738, help='the server to connect to (default "64738")')
+argParser.add_argument('--server', nargs='?',  dest='server', default='localhost', help='the server to connect to (default "localhost")')
+argParser.add_argument('--username', dest='username', help='the username of the client')
 
+###############################################################################
+## Global Variables
+###############################################################################
+MyArgs = argParser.parse_args()
 IsConnected = Event()
+ExitNowPlease = Event()
+
+###############################################################################
+## Event Handlers
+###############################################################################
+def OnCtrlC(signum, frame):
+    ExitNowPlease.set()
+
 def OnConnected():
         IsConnected.set()
         print("Connected.")
-        mumble.channels.find_by_name(args.channel).move_in()
+        mumble.channels.find_by_name(MyArgs.channel).move_in()
 
 def OnDisconnected():
     IsConnected.clear()
     print("Server Disconnected.")
-    print("Attempting to reconnect...")
 
+    if not ExitNowPlease.is_set():
+        print("Attempting to reconnect...")
+
+###############################################################################
+## Main Program
+###############################################################################
 pyAudioBufferSize=1024
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=48000, input=True, output=False, frames_per_buffer=pyAudioBufferSize)
+signal.signal(signal.SIGINT, OnCtrlC)
 
-mumble = pymumble_py3.Mumble(args.server, args.username, password=args.password, port=args.port, certfile=args.certfile, keyfile=args.certkey, reconnect=True)
+mumble = pymumble_py3.Mumble(MyArgs.server, MyArgs.username, password=MyArgs.password, port=MyArgs.port, certfile=MyArgs.certfile, keyfile=MyArgs.certkey, reconnect=True)
 mumble.set_application_string("Rumble de K9CTS")
 mumble.start()
 mumble.callbacks.set_callback(PYMUMBLE_CLBK_CONNECTED, OnConnected)
 mumble.callbacks.set_callback(PYMUMBLE_CLBK_DISCONNECTED, OnDisconnected)
 mumble.is_ready()
 
-while True:
+while not ExitNowPlease.is_set():
     data = stream.read(pyAudioBufferSize, exception_on_overflow=False)
     if IsConnected.is_set():
         mumble.sound_output.add_sound(data)
@@ -48,4 +66,3 @@ while True:
 stream.stop_stream()
 stream.close()
 p.terminate()
-
